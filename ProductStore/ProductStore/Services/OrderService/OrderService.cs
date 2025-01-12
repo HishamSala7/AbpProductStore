0e210c4623f2fecb719bf6aa3e4915aa0ae93f8e
Hisham Salah <H.Salah@ASIS-Egypt.local>
2025-01-12 17:39:38 +0200

    Services Completed

--- ProductStore/ProductStore/Services/OrderService/OrderService.cs
+++ ProductStore/ProductStore/Services/OrderService/OrderService.cs
@@ -1,6 +1,57 @@
-﻿namespace ProductStore.Services.OrderService
+﻿using ProductStore.Entities;
+using ProductStore.Services.Dtos.OrderDtos;
+using ProductStore.Services.Dtos.ProductDtos;
+using ProductStore.Services.Dtos.ReadProductDtos;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.ObjectMapping;
+
+namespace ProductStore.Services.OrderService
 {
     public class OrderService : ProductStoreAppService, IOrderService
     {
+        private readonly IRepository<Order, int> _OrderRepo;
+
+        public OrderService(IRepository<Order, int> orderRepo)
+        {
+            _OrderRepo = orderRepo;
+        }
+
+        public async Task<List<ReadOrderDto>> GetAllAsync()
+        {
+            var orders = await _OrderRepo.GetListAsync();
+
+            return ObjectMapper.Map<List<Order>, List<ReadOrderDto>>(orders);
+        }
+
+        public async Task<ReadOrderDto> GetById(int id)
+        {
+            var order = await _OrderRepo.GetAsync(id);
+
+            return ObjectMapper.Map<Order, ReadOrderDto>(order);
+        }
+
+        public async Task<ReadOrderDto> CreateAsync(CreateOrderDto orderDto)
+        {
+            var insertedOrder = await _OrderRepo.InsertAsync(ObjectMapper.Map<CreateOrderDto, Order>(orderDto));
+
+            return ObjectMapper.Map<Order, ReadOrderDto>(insertedOrder);
+        }
+
+        public async Task<ReadOrderDto> UpdateAsync(UpdateOrderDto orderDto)
+        {
+            var orderDb = await _OrderRepo.GetAsync(orderDto.Id);
+
+            ObjectMapper.Map<UpdateOrderDto, Order>(orderDto, orderDb);
+
+            var updatedOrder = await _OrderRepo.UpdateAsync(orderDb);
+
+            return ObjectMapper.Map<Order, ReadOrderDto>(updatedOrder);
+        }
+
+        //public async Task DeleteAsync(int id)
+        //{
+        //    await _orderRepo.DeleteAsync(id);
+        //}
+
     }
 }
--- ProductStore/ProductStore/Services/ProductService/IProductService.cs
+++ ProductStore/ProductStore/Services/ProductService/IProductService.cs
@@ -1,8 +1,16 @@
-﻿using Volo.Abp.Application.Services;
+﻿using AutoMapper.Internal.Mappers;
+using ProductStore.Entities;
+using ProductStore.Services.Dtos.ProductDtos;
+using ProductStore.Services.Dtos.ReadProductDtos;
+using Volo.Abp.Application.Services;
 
 namespace ProductStore.Services.ProductService
 {
     public interface IProductService : IApplicationService
     {
+        Task<List<ReadProductDto>> GetAllAsync();
+        Task<ReadProductDto> GetById(int id);
+        Task<ReadProductDto> CreateAsync(CreateProductDto productDto);
+        Task<ReadProductDto> UpdateAsync(UpdateProductDto productDto);
     }
 }
